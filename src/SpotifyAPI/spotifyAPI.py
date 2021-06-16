34c57332a68981aa729306352d3f47d1836d87ba
Kevin Guillet <kevin.guillet@epita.fr>
2021-06-16 19:19:46 +0200

    [API][OAUTH]: implement access with spotify oauth
    And add method to gt new released song

--- src/SpotifyAPI/spotifyAPI.py
+++ src/SpotifyAPI/spotifyAPI.py
@@ -1,19 +1,50 @@
 import spotipy
 import spotipy.util as util
+from spotipy.oauth2 import SpotifyOAuth
 
 
 class APIManager:
 
-    def __init__(self):
-        self.user = 'cloud_441'
-        token = util.prompt_for_user_token(self.user,
-                'user-library-read',
-                client_id='c8464bcbd6ba4e5f97b3c7e0f5a5a3be',
-                client_secret='91cfce9387b0463ab0e5ef2043d0a9c9',
-                redirect_uri='http://localhost/')
-        print("Authorization configuration is done.")
+    def __init__(self, user='spotify_oauth'):
+        self.user = user
 
-        if token:
-            self.api = spotipy.Spotify(auth=token)
+        if (user == 'spotify_oauth'):
+            token = SpotifyOAuth(client_id='c8464bcbd6ba4e5f97b3c7e0f5a5a3be',
+                    client_secret='91cfce9387b0463ab0e5ef2043d0a9c9',
+                    redirect_uri='http://localhost/')
+
+            if token:
+                self.api = spotipy.Spotify(auth_manager=token)
+            else:
+                print("Can't get token for", username)
+
         else:
-            print("Can't get token for", username)
+            token = util.prompt_for_user_token(self.user,
+                    'user-library-read',
+                    client_id='c8464bcbd6ba4e5f97b3c7e0f5a5a3be',
+                    client_secret='91cfce9387b0463ab0e5ef2043d0a9c9',
+                    redirect_uri='http://localhost/')
+
+            if token:
+                self.api = spotipy.Spotify(auth=token)
+            else:
+                print("Can't get token for", username)
+
+
+    def get_new_released(self):
+        if not (self.user == 'spotify_oauth'):
+            print(f'Error: Impossible to load new release song with user {self.user}')
+            return
+
+        response = self.api.new_releases()
+
+        while response:
+            albums = response['albums']
+            for i, item in enumerate(albums['items']):
+                print(albums['offset'] + i, item['name'])
+
+                if albums['next']:
+                    response = self.api.next(albums)
+                else:
+                    response = None
+
--- src/spotifyPredictor.py
+++ src/spotifyPredictor.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 
+import sys
 import SpotifyAPI
 
 
-def run():
-    api_manager = SpotifyAPI.APIManager()
+def run(argv):
+
+    api_manager = SpotifyAPI.APIManager() if (len(argv[1:]) == 0) else SpotifyAPI.APIManager(user=argv[1])
     if (api_manager.api):
         print(f'The API connection is established with user {api_manager.user}.')
+        api_manager.get_new_released()
     else:
         print(f'Error: Impossible to established API connection with user {api_manager.user}')
 
 
 
 # Input of the project:
-run()
+run(sys.argv)
